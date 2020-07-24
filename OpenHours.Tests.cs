cdf06951865457e8b6893c365ca9ae96ca850992
Charles Neau <charles63500@gmail.com>
2020-07-24 22:18:45 +0100

    tests passing

--- OpenHours.Tests.cs
+++ OpenHours.Tests.cs
@@ -1,16 +1,35 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Chneau.Time
 {
     public class Tests
     {
-        [Fact]
-        public void TestTwo()
-        {
-            var oh = new OpenHours();
-            Assert.Equal(2, oh.Two()); // success
-            Assert.Equal(2, oh.Two() + 40); // fails
-        }
+        [Theory]
+        [InlineData("Mo-Fr 10:00-12:00,12:30-16:00", "mo-fr 10:00-12:00,12:30-16:00")]
+        [InlineData("Mo-Fr 10:00-12:00, 12:30-16:00", "mo-fr 10:00-12:00,12:30-16:00")]
+        [InlineData("Mo-Fr 10:00-12:00 ,12:30-16:00", "mo-fr 10:00-12:00,12:30-16:00")]
+        [InlineData("Mo-Fr 10:00-12:00 , 12:30-16:00", "mo-fr 10:00-12:00,12:30-16:00")]
+        [InlineData(" Mo-Fr 10:00-12:00,12:30-16:00", "mo-fr 10:00-12:00,12:30-16:00")]
+        [InlineData("Mo-Fr 10:00-12:00,12:30-16:00 ", "mo-fr 10:00-12:00,12:30-16:00")]
+        [InlineData(" Mo-Fr 10:00-12:00,12:30-16:00 ", "mo-fr 10:00-12:00,12:30-16:00")]
+        [InlineData(" 	Mo-Fr 10:00-12:00,12:30-16:00	 ", "mo-fr 10:00-12:00,12:30-16:00")]
+        [InlineData(" 	 	Mo-Fr 	 10:00-12:00,12:30-16:00 	 	 ", "mo-fr 10:00-12:00,12:30-16:00")]
+        public void TestCleanInput(string input, string expected) =>
+            Assert.Equal(expected, OpenHours.CleanInput(input));
+
+        [Theory]
+        [InlineData("mo", new int[] { 1 })]
+        [InlineData("mo,mardi", new int[] { 1 })]
+        [InlineData("we,fr", new int[] { 3, 5 })]
+        [InlineData("we-fr", new int[] { 3, 4, 5 })]
+        [InlineData("mo,we-fr,su", new int[] { 0, 1, 3, 4, 5 })]
+        [InlineData("mo-pl", new int[] { })]
+        [InlineData("pl,mo", new int[] { 1 })]
+        [InlineData("fr-mo", new int[] { 0, 1, 5, 6 })]
+        [InlineData("mo-tu,tu,tu-fr,fr", new int[] { 1, 2, 3, 4, 5 })]
+        public void InternalTestSimplifyDays(string input, int[] expected) => Assert.Equal(expected, OpenHours.SimplifyDays(input));
+
     }
 }
--- OpenHours.cs
+++ OpenHours.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace Chneau.Time
 {
     public class OpenHours
     {
-        public int Two()
+        internal List<UInt32> points = new List<UInt32>(); // 	0 to 4_294_967_295, which is good since > 60*60*24*7 = 604_800
+        private static Dictionary<string, int> DAYS = new Dictionary<string, int> { { "su", 0 }, { "mo", 1 }, { "tu", 2 }, { "we", 3 }, { "th", 4 }, { "fr", 5 }, { "sa", 6 } };
+
+        internal static String CleanInput(String input)
         {
-            return 2;
+            var result = input.ToLower();
+            result = result.Trim();
+            result = result.Replace(", ", ",");
+            result = result.Replace(" ,", ",");
+            result = Regex.Replace(result, @"\s+", " ");
+            return result;
+        }
+
+        internal static IEnumerable<int> SimplifyDays(String input)
+        {
+            var days = new HashSet<int>();
+            foreach (var comma in input.Split(","))
+            {
+                if (comma.Contains("-"))
+                {
+                    var split = comma.Split("-");
+                    if (split.Length == 2 && (DAYS.ContainsKey(split[0]) && DAYS.ContainsKey(split[1])))
+                    {
+                        var from = DAYS.GetValueOrDefault(split[0]);
+                        var to = DAYS.GetValueOrDefault(split[1]);
+                        do
+                        {
+                            days.Add(from);
+                            from++;
+                            if (from == 7)
+                            {
+                                from = 0;
+                            }
+                        } while (from != to);
+                        days.Add(from);
+                    } // else ignore
+                }
+                else
+                {
+                    if (DAYS.ContainsKey(comma))
+                    {
+                        days.Add(DAYS.GetValueOrDefault(comma));
+                    }
+                }
+            }
+            var sorted = new List<int>(days);
+            sorted.Sort();
+            return sorted;
+        }
+
+        internal void BuildTimes(String input)
+        {
+
+        }
+
+        public static OpenHours From(String input)
+        {
+            var oh = new OpenHours();
+            oh.BuildTimes(input);
+            return oh;
         }
     }
 }
